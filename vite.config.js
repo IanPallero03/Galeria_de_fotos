0d3080b35030a89cd0a227d6bea6e71aafd5b49f
IanPallero03 <119908378+IanPallero03@users.noreply.github.com>
2025-08-01 21:36:32 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Galeria_de_fotos/', // <-- este debe coincidir con el nombre del repo
   plugins: [react()],
 })
+
